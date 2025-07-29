ba75c5a7895094c603ee0f92bd7a86b551804475
roasted-oolong <65867683+roasted-oolong@users.noreply.github.com>
2025-07-29 15:54:21 -0500

    Create Book constructor

--- js-file.js
+++ js-file.js
@@ -2,3 +2,20 @@ const p = document.createElement("p");
 p.textContent = "Hey I'm red!";
 p.style.cssText = "color: blue";
 document.body.appendChild(p);
+
+function Book(title, author, pages, read) {
+    if (!new.target) {
+        throw Error("You must use the 'new' operator to call the constructor")
+    }
+
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function () {
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`
+    }
+}
+
+const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'not read yet')
+console.log(theHobbit.info());
