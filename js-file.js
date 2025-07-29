42689aa9edf7b87d153e95f40142d6e7ff3648ba
roasted-oolong <65867683+roasted-oolong@users.noreply.github.com>
2025-07-29 15:59:23 -0500

    Add myLibrary array
    The myLibrary array is a blank array that will store any books from the function addBookToLibrary()

--- js-file.js
+++ js-file.js
@@ -3,6 +3,7 @@ p.textContent = "Hey I'm red!";
 p.style.cssText = "color: blue";
 document.body.appendChild(p);
 
+const myLibrary = [];
 function Book(title, author, pages, read) {
     if (!new.target) {
         throw Error("You must use the 'new' operator to call the constructor")
