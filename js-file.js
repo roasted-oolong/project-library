88acd8c7fa2bd30ebeff38129e5106e71e957ef1
roasted-oolong <65867683+roasted-oolong@users.noreply.github.com>
2025-07-30 11:51:11 -0500

    [In-Progress] Add displayBook function
    The function is intended to display books in the library with a card.
    Function is not working yet and is in progress.

--- js-file.js
+++ js-file.js
@@ -3,7 +3,9 @@ p.textContent = "Hey I'm red!";
 p.style.cssText = "color: blue";
 document.body.appendChild(p);
 
-const myLibrary = [];
+const myLibrary = [
+    newBook('The Hobbit', 'J.R.R. Tolkien', 295, 'unread')
+];
 function Book(title, author, pages, read) {
     if (!new.target) {
         throw Error("You must use the 'new' operator to call the constructor")
@@ -23,3 +25,22 @@ function addBookToLibrary(title, author, pages, read) {
 
 addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, 'not read yet')
 console.log(myLibrary)
+
+function displayBook() {
+    const container = document.getElementById('book-container')
+    container.innerHTML = ''
+
+    myLibrary.forEach((book) => {
+        const card = document.createElement('div')
+        card.classList.add('book-card')
+        
+        card.innerHTML = `
+            <h3>${newBook.tile}</h3>
+            <p><strong>Author:</strong>${newBook.author}</p>
+            <p><strong>Pages:</strong>${book.pages}</p>
+            <p><strong>Status:</strong>${book.read}</p>
+        `
+
+        document.appendChild(card);
+    })
+}
